568982cd2ba663252e6f3bb14fe38dd8e3ee60c7
nitish1309 <n.svastav@gmail.com>
2022-07-30 18:36:42 +0530

    hello

---
+++ src/module1/Topic1.java
@@ -0,0 +1,15 @@
+package module1;
+
+import org.testng.annotations.Test;
+
+
+public class Topic1 {
+	@Test
+	public void TestCase1() {
+		System.out.println("TestCase1 Executed");
+	}
+	@Test
+	public void TestCase2() {
+		System.out.println("TestCase2 Executed");
+	}
+}
---
+++ src/module1/Topic4.java
@@ -0,0 +1,14 @@
+package module1;
+
+import org.testng.annotations.Test;
+
+public class Topic4 {
+	@Test
+	public void TestCase7() {
+		System.out.println("TestCase7 Executed");
+	}
+	@Test
+	public void TestCase8() {
+		System.out.println("TestCase8 Executed");
+	}
+}
